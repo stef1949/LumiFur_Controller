412a252503792e161de1397aae37126ca47bb97e
Stefan Ritchie <stef1949.sr@gmail.com>
2025-12-28 07:17:12 +0000

    Refactor BLE server callbacks to conditionally log connection events based on DEBUG_BLE flag

--- src/ble/ble_server.cpp
+++ src/ble/ble_server.cpp
@@ -4,7 +4,6 @@
 void ServerCallbacks::onConnect(NimBLEServer *pServer, NimBLEConnInfo &connInfo)
 {
     deviceConnected = true;
-    Serial.printf("Client connected: %s\n", connInfo.getAddress().toString().c_str());
 
     /**
      *  We can use the connection handle here to ask for different connection parameters.
@@ -15,39 +14,53 @@ void ServerCallbacks::onConnect(NimBLEServer *pServer, NimBLEConnInfo &connInfo)
      *  Timeout: 10 millisecond increments.
      */
     pServer->updateConnParams(connInfo.getConnHandle(), 24, 48, 0, 180);
+
+#if DEBUG_BLE
+    Serial.printf("Client connected: %s\n", connInfo.getAddress().toString().c_str());
+#endif
 }
 
 void ServerCallbacks::onPairingRequest(NimBLEServer *pServer, NimBLEConnInfo &connInfo)
 {
     devicePairing = true;
+#if DEBUG_BLE
     Serial.printf("Pairing request from: %s\n", connInfo.getAddress().toString().c_str());
+#endif
 }
 
 void ServerCallbacks::onDisconnect(NimBLEServer *pServer, NimBLEConnInfo &connInfo, int reason)
 {
     deviceConnected = false;
-    Serial.println("Client disconnected - advertising");
     NimBLEDevice::startAdvertising();
+#if DEBUG_BLE
+    Serial.println("Client disconnected - advertising");
+#endif
 }
 
 void ServerCallbacks::onMTUChange(uint16_t MTU, NimBLEConnInfo &connInfo)
 {
+#if DEBUG_BLE
     Serial.printf("MTU updated: %u for connection ID: %u\n", MTU, connInfo.getConnHandle());
+#endif
 }
 
 uint32_t ServerCallbacks::onPassKeyDisplay()
 {
-    Serial.printf("Server Passkey Display\n");
     /**
      * This should return a random 6 digit number for security
      *  or make your own static passkey as done here.
      */
+#if DEBUG_BLE
+    Serial.printf("Server Passkey Display\n");
+#endif
     return 123456;
 }
 
 void ServerCallbacks::onConfirmPassKey(NimBLEConnInfo &connInfo, uint32_t pass_key)
 {
+#if DEBUG_BLE
     Serial.printf("The passkey YES/NO number: %" PRIu32 "\n", pass_key);
+#endif
     /** Inject false if passkeys don't match. */
     NimBLEDevice::injectConfirmPasskey(connInfo, true);
 }
